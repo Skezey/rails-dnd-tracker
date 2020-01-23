8bb07f62f226703559889178dc8ff7a42dc53bd8
Scott Young <youngscottj@gmail.com>
2020-01-22 23:50:57 -0700

    add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'api/private' => 'private#private'
   get 'api/private-scoped' => 'private#private_scoped'
+  namespace :api do
+    resources :users
+
+  end
+
 
   get '*other', to: 'static#index'
 end
