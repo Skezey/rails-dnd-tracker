ff085eab47aa24a332fb29592b387296beefa42c
Scott Young <syoung@instructure.com>
2020-03-29 21:07:35 -0600

    remove reduntant columns from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_015926) do
+ActiveRecord::Schema.define(version: 2020_03_30_030506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,10 +43,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_015926) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "uid"
-    t.string "email"
-    t.string "picture"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
