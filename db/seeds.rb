8c92c8f6638da3f4d26903b9121c8a0ae80748b2
Scott Young <syoung@instructure.com>
2020-05-05 17:40:52 -0600

    add attributes to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ def create_characters
   races = %w[Elf Dwarf Halfling Human Changling Orc]
   classes = %w[Fighter Monk Druid Rogue Wizard Sorcerer Warlock Bard]
   10.times do |i|
-    Character.create(
+    character = Character.create(
       name: Faker::FunnyName.two_word_name,
       race: races.sample,
       char_class: classes.sample,
@@ -12,7 +12,17 @@ def create_characters
       traits: 'n/a',
       user_id: 1
     )
+    CharacterAttribute.create(
+      strength: rand(1..20),
+      charisma: rand(1..20),
+      intelligence: rand(1..20),
+      dexterity: rand(1..20),
+      constitution: rand(1..20),
+      wisdom: rand(1..20),
+      character_id: character.id
+    )
   end
+  puts 'created 10 users with attributes'
 end
 
 create_characters
